340e12d7c5d22b26c2804241d6f1bdf8134de34b
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-09-17 10:44:21 +0200

    Fix reading ogranization cluster id

--- projects/lib/portal-options/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './resource';
-export * from './luigi-context';
-export * from './luigi-node';
-export * from './constants';
--- projects/lib/services/resource/resource.service.ts
+++ projects/lib/services/resource/resource.service.ts
@@ -1,13 +1,9 @@
+import { AccountInfo, Resource, ResourceDefinition } from '../models';
 import { ApolloFactory } from './apollo-factory';
 import { ResourceNodeContext } from './resource-node-context';
 import { Injectable, inject } from '@angular/core';
 import { TypedDocumentNode } from '@apollo/client/core';
 import { LuigiCoreService } from '@openmfp/portal-ui-lib';
-import {
-  AccountInfo,
-  Resource,
-  ResourceDefinition,
-} from '@platform-mesh/portal-ui-lib/portal-options/models';
 import {
   getValueByPath,
   replaceDotsAndHyphensWithUnderscores,
