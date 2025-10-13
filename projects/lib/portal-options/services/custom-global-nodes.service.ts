63ad21357e28224a19cdc79aee12f1b545c721b3
Sobyt483 <kravczov.andrian@mail.ru>
2025-10-13 18:08:03 +0400

    feat: add user nodes

--- projects/lib/portal-options/services/custom-global-nodes.service.ts
+++ projects/lib/portal-options/services/custom-global-nodes.service.ts
@@ -1,8 +1,9 @@
-import { inject } from '@angular/core';
-import { CustomGlobalNodesService, I18nService } from '@openmfp/portal-ui-lib';
 import { kcpRootOrgsPath } from '../models/constants';
 import { PortalNodeContext } from '../models/luigi-context';
 import { PortalLuigiNode } from '../models/luigi-node';
+import { inject } from '@angular/core';
+import { CustomGlobalNodesService, I18nService } from '@openmfp/portal-ui-lib';
+
 
 export class CustomGlobalNodesServiceImpl implements CustomGlobalNodesService {
   private i18nService = inject(I18nService);
@@ -15,7 +16,8 @@ export class CustomGlobalNodesServiceImpl implements CustomGlobalNodesService {
         hideFromNav: true,
         hideSideNav: true,
         order: '1001',
-        viewUrl: '/assets/platform-mesh-portal-ui-wc.js#organization-management',
+        viewUrl:
+          '/assets/platform-mesh-portal-ui-wc.js#organization-management',
         webcomponent: {
           selfRegistered: true,
         },
@@ -23,6 +25,48 @@ export class CustomGlobalNodesServiceImpl implements CustomGlobalNodesService {
           translationTable: this.i18nService.translationTable,
           kcpPath: kcpRootOrgsPath,
         } as PortalNodeContext,
+      },
+      {
+        label: 'Users',
+        pathSegment: 'users',
+        showBreadcrumbs: false,
+        hideSideNav: true,
+        hideFromNav: true,
+        entityType: 'global',
+        context: {} as PortalNodeContext,
+        children: [
+          {
+            context: {
+              profileUserId: ':profileUserId',
+            } as unknown as PortalNodeContext,
+            defineEntity: {
+              id: 'user',
+              contextKey: 'profileUserId',
+              label: 'User',
+              pluralLabel: 'Users',
+            },
+            label: 'User',
+            pathSegment: ':profileUserId',
+            hideSideNav: true,
+            hideFromNav: true,
+            children: [
+              {
+                label: 'Overview',
+                pathSegment: 'overview',
+                entityType: 'user',
+                hideSideNav: true,
+                hideFromNav: true,
+                defineEntity: {
+                  id: 'overview',
+                },
+                children: [],
+                compound: {
+                  children: [],
+                },
+              },
+            ],
+          },
+        ],
       },
     ];
   }
--- projects/lib/portal-options/services/user-profile-config.service.ts
+++ projects/lib/portal-options/services/user-profile-config.service.ts
@@ -1,11 +1,23 @@
-import { Injectable } from '@angular/core';
-import { UserProfile, UserProfileConfigService } from '@openmfp/portal-ui-lib';
+import { Injectable, inject } from '@angular/core';
+import {
+  AuthService,
+  UserProfile,
+  UserProfileConfigService,
+} from '@openmfp/portal-ui-lib';
 
 @Injectable({ providedIn: 'root' })
 export class UserProfileConfigServiceImpl implements UserProfileConfigService {
+  private authService = inject(AuthService);
   async getProfile(): Promise<UserProfile> {
+    const { userId } = this.authService.getUserInfo();
+
     return {
       items: [
+        {
+          label: 'PROFILE_PROFILE',
+          icon: 'customer',
+          link: `/users/${userId}/overview`,
+        },
         {
           label: 'PROFILE_ORGANIZATION',
           icon: 'building',
