ed2a14c4e83914f72292bc786d2464cc2696f07c
Grzegorz Krajniak <gkrajniak@gmail.com>
2025-11-04 10:44:10 +0100

    Add `kcpPath` to Luigi context configuration and processing (#98)
    * Add `entityName` and `accountPath` to Luigi context configuration and processing
    
    * Add `entityName` and `accountPath` to Luigi context configuration and processing
    
    * Add kcpPath to node context
    
    * Add kcpPath to node context
    
    * Add kcpPath to node context
    
    * fix tests
    
    * fix tests
    
    * Adjust kcp path calculation
    
    * fix tests
    
    * Add kcpPath to Luigi context configuration and processing
    
    * Adjust kcp path calculation
    
    * fix tests
    
    ---------
    
    Co-authored-by: Grzegorz Krajniak <grzegorz.krajniak@sap.com>

--- projects/lib/services/resource/gateway.service.ts
+++ projects/lib/services/resource/gateway.service.ts
@@ -2,7 +2,6 @@ import { ResourceNodeContext } from './resource-node-context';
 import { Injectable, inject } from '@angular/core';
 import { LuigiCoreService } from '@openmfp/portal-ui-lib';
 
-
 @Injectable({ providedIn: 'root' })
 export class GatewayService {
   private luigiCoreService = inject(LuigiCoreService);
@@ -12,8 +11,7 @@ export class GatewayService {
     readFromParentKcpPath = false,
   ) {
     const gatewayUrl = nodeContext.portalContext.crdGatewayApiUrl;
-    const currentKcpPath = this.getCurrentKcpPath(gatewayUrl);
-
+    const currentKcpPath = gatewayUrl.match(/\/([^\/]+)\/graphql$/)?.[1] || '';
     return gatewayUrl?.replace(
       currentKcpPath,
       this.resolveKcpPath(nodeContext, readFromParentKcpPath),
@@ -31,35 +29,21 @@ export class GatewayService {
   public resolveKcpPath(
     nodeContext: ResourceNodeContext,
     readFromParentKcpPath = false,
-  ) {
-    if (nodeContext.kcpPath) {
-      return nodeContext.kcpPath;
-    }
-
+  ): string {
     const gatewayUrl = nodeContext.portalContext.crdGatewayApiUrl;
-    const currentKcpPath = this.getCurrentKcpPath(gatewayUrl);
-    const lastIndex = currentKcpPath.lastIndexOf(':');
 
-    if (readFromParentKcpPath && lastIndex !== -1) {
-      return currentKcpPath.slice(0, lastIndex);
+    let kcpPath = gatewayUrl.match(/\/([^\/]+)\/graphql$/)?.[1] || '';
+    if (nodeContext.kcpPath) {
+      kcpPath = nodeContext.kcpPath;
     }
 
-    return currentKcpPath;
-  }
-
-  private getCurrentKcpPath(gatewayUrl: string): string {
-    const kcpPathRegexp = /\/([^\/]+)\/graphql$/;
-    const currentKcpPath = gatewayUrl.match(kcpPathRegexp)?.[1];
-
-    if (!currentKcpPath) {
-      this.luigiCoreService.showAlert({
-        text: 'Could not get current KCP path from gateway URL',
-        type: 'error',
-      });
-
-      return '';
+    if (readFromParentKcpPath) {
+      const lastIndex = kcpPath.lastIndexOf(':');
+      if (lastIndex !== -1) {
+        kcpPath = kcpPath.slice(0, lastIndex);
+      }
     }
 
-    return currentKcpPath;
+    return kcpPath;
   }
 }
