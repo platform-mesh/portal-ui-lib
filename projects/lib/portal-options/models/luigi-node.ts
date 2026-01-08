2f8e35bc1fd01cb4ea99573f25649b5deb9da4de
Sobyt483 <kravczov.andrian@mail.ru>
2026-01-08 15:50:09 +0400

    feat: update interface

--- projects/lib/portal-options/models/luigi-node.ts
+++ projects/lib/portal-options/models/luigi-node.ts
@@ -1,7 +1,20 @@
 import { PortalNodeContext } from './luigi-context';
+import type { EntityDefinition } from '@openmfp/portal-ui-lib';
 import { LuigiNode } from '@openmfp/portal-ui-lib';
 
 export interface PortalLuigiNode extends LuigiNode {
+  defineEntity?: PMEntityDefinition;
   context: PortalNodeContext;
   parent?: PortalLuigiNode;
+}
+
+export interface PMEntityDefinition extends EntityDefinition {
+  graphqlEntity?: GraphqlEntityDefinition;
+}
+
+export interface GraphqlEntityDefinition {
+  group: string;
+  kind: string;
+  query: string;
+  version: string;
 }
