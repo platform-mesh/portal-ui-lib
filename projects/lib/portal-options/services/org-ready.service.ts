809d997a667a001818e227251a60e4e77479576c
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2026-01-26 13:47:59 +0100

    Fix setting of luigi storage type

--- projects/lib/portal-options/services/org-ready.service.ts
+++ projects/lib/portal-options/services/org-ready.service.ts
@@ -1,49 +1,54 @@
-import { inject, Injectable } from '@angular/core';
-import { AuthService, ConfigService, EnvConfigService } from '@openmfp/portal-ui-lib';
+import { Injectable, inject } from '@angular/core';
+import {
+  AuthService,
+  ConfigService,
+  EnvConfigService,
+} from '@openmfp/portal-ui-lib';
 import { ResourceService } from '@platform-mesh/portal-ui-lib/services';
-import { exhaustMap, filter, Subject, tap } from 'rxjs';
+import { Subject, exhaustMap, filter, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class OrganizationReadyService {
-    private configService = inject(ConfigService);
-    private envConfigService = inject(EnvConfigService);
-    private resourceService = inject(ResourceService);
-    private authService = inject(AuthService);
+  private configService = inject(ConfigService);
+  private envConfigService = inject(EnvConfigService);
+  private resourceService = inject(ResourceService);
+  private authService = inject(AuthService);
 
-    private isReady = false;
-    private check$ = new Subject<void>();
+  private isReady = false;
+  private check$ = new Subject<void>();
 
-    constructor() {
-      this.initChecks();
+  constructor() {
+    void this.initChecks();
+  }
+
+  private async initChecks() {
+    const portalConfig = await this.configService.getPortalConfig();
+    const { idpName } = await this.envConfigService.getEnvConfig();
+
+    if (idpName === 'welcome') {
+      return;
     }
 
-    private async initChecks() {
-      const portalConfig = await this.configService.getPortalConfig();
-      const envIdpName = (await this.envConfigService.getEnvConfig()).idpName;
-
-      if(envIdpName === 'welcome') {
-        return;
-      }
-
-      this.check$
+    this.check$
       .pipe(
         filter(() => !this.isReady),
         exhaustMap(() =>
           this.resourceService.readOrganizationReady({
-              portalContext: {
-                  crdGatewayApiUrl: portalConfig.portalContext['crdGatewayApiUrl'],
-              },
-              token: this.authService.getToken(),
-              accountId: envIdpName,
+            portalContext: {
+              crdGatewayApiUrl: portalConfig.portalContext['crdGatewayApiUrl'],
+            },
+            token: this.authService.getToken(),
+            accountId: idpName,
           }),
-          ),
-          tap((isReady) => {
-              this.isReady = isReady;
-          }),
-        ).subscribe()
-    }
+        ),
+        tap((isReady) => {
+          this.isReady = isReady;
+        }),
+      )
+      .subscribe();
+  }
 
-    public checkOrganizationReady() {
-        this.check$.next();
-    }
+  public checkOrganizationReady() {
+    this.check$.next();
+  }
 }
