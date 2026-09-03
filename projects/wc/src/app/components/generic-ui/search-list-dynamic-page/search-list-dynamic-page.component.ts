ce31799b5d591bc67292ae533999de3e57c2dcbf
Sobyt483 <andrianingomel@gmail.com>
2026-09-03 16:14:29 +0400

    feat: first implementation
    Signed-off-by: Sobyt483 <andrianingomel@gmail.com>

--- projects/wc/src/app/components/generic-ui/search-list-dynamic-page/search-list-dynamic-page.component.ts
+++ projects/wc/src/app/components/generic-ui/search-list-dynamic-page/search-list-dynamic-page.component.ts
@@ -1,18 +1,33 @@
+import { executeButtonAction } from '../../../utils/field-definition.utils';
+import { CreateResourceModal } from '../create-resource-modal/create-resource-modal.component';
+import { ResourceLogo } from '../resource-logo/resource-logo.component';
 import {
   ChangeDetectionStrategy,
   Component,
   ViewEncapsulation,
   computed,
+  inject,
   input,
+  viewChild,
 } from '@angular/core';
 import {
   DynamicPage,
   DynamicPageHeader,
   DynamicPageTitle,
 } from '@fundamental-ngx/ui5-webcomponents-fiori';
+import { Button } from '@fundamental-ngx/ui5-webcomponents/button';
 import { Title } from '@fundamental-ngx/ui5-webcomponents/title';
 import { LuigiClient } from '@luigi-project/client/luigi-element';
-import { ResourceNodeContext } from '@platform-mesh/portal-ui-lib/services';
+import { ResourceField, ResourceFieldButtonClickEvent } from '@openmfp/ngx';
+import {
+  PlatformMeshFieldDefinition,
+  Resource,
+} from '@platform-mesh/portal-ui-lib/models';
+import {
+  ResourceNodeContext,
+  ResourceService,
+} from '@platform-mesh/portal-ui-lib/services';
+import { resourceActionAllowed } from '@platform-mesh/portal-ui-lib/utils';
 
 @Component({
   selector: 'pm-search-list-dynamic-page',
@@ -20,7 +35,16 @@ import { ResourceNodeContext } from '@platform-mesh/portal-ui-lib/services';
   templateUrl: './search-list-dynamic-page.component.html',
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [DynamicPage, DynamicPageTitle, DynamicPageHeader, Title],
+  imports: [
+    DynamicPage,
+    DynamicPageTitle,
+    DynamicPageHeader,
+    Title,
+    Button,
+    ResourceLogo,
+    ResourceField,
+    CreateResourceModal,
+  ],
 })
 export class SearchListDynamicPage {
   LuigiClient = input.required<LuigiClient>();
@@ -40,4 +64,55 @@ export class SearchListDynamicPage {
       this.resourceDefinition()?.ui?.listView?.resourceDescription?.label ??
       `This page displays the created ${this.resourceDefinition()?.entityCollection} in your environment`,
   );
+
+  actions = computed<PlatformMeshFieldDefinition[]>(
+    () => this.resourceDefinition()?.ui?.listView?.actions ?? [],
+  );
+
+  hasUiCreateViewFields = computed(
+    () => !!this.resourceDefinition()?.ui?.createView?.fields?.length,
+  );
+
+  createFields = computed<PlatformMeshFieldDefinition[]>(
+    () => this.resourceDefinition()?.ui?.createView?.fields ?? [],
+  );
+
+  canCreate = computed(() => this.canDo('create'));
+
+  private resourceService = inject(ResourceService);
+  private createModal = viewChild<CreateResourceModal>('createModal');
+
+  openCreateModal(): void {
+    void this.createModal()?.open();
+  }
+
+  onCreateSubmit(value: Resource): void {
+    const resourceDefinition = this.resourceDefinition();
+    if (!resourceDefinition) return;
+    this.resourceService
+      .create(value, resourceDefinition, this.context())
+      .subscribe({
+        next: (result) => {
+          this.createModal()?.close();
+          // TODO(#593): refresh the list once the table is added (re-run OpenSearch query)
+          console.debug('Resource created', result);
+        },
+      });
+  }
+
+  genericActionHandler(event: ResourceFieldButtonClickEvent<Resource>): void {
+    executeButtonAction(this.LuigiClient(), event.field, event.resource);
+  }
+
+  canDo(action: string | undefined): boolean {
+    if (!action) {
+      return true;
+    }
+
+    return resourceActionAllowed(
+      this.context().portalPermissions,
+      this.resourceDefinition()?.permissionsDefinition?.resource,
+      action,
+    );
+  }
 }
